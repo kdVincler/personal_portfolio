5c7981c4b6b82bb2dcb3cee49e703b041b8c93d6
Konrad Dominik Vincler <konrad.vincler@gmail.com>
2024-03-28 21:39:03 +0000

    Implemented the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,15 @@ import '../css/Navbar.css';
 
 function Navbar () {
     return(
-        <>
-        
-        </>
+        <nav>
+            <h1 className='almostLogo'><a href=''>Konrad Vincler</a></h1>
+            <ul>
+                <li><a href="#">Education</a></li>
+                <li><a href="#">Experience</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
     )
 }
 
