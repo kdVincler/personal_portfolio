18ec89cd99cd15b1fffd5abc3936782e87390d37
Konrad Dominik Vincler <konrad.vincler@gmail.com>
2024-03-29 16:23:53 +0000

    Implemented Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,10 +8,59 @@ function Education ({showing}) {
     if (!showing) return null;
     
     return(
-        <>
-        
-        </>
+        <a id='edu'>
+            <section className='tile education'>
+                <h1 className='sectionHead edu'>
+                    Education
+                </h1>
+                <EduCard
+                    time="2022 - Present"
+                    school="Queen Mary University of London"
+                    degree="BSc (Hons) Computer Science"
+                    comments="1st year - 88% <br /> 
+                                2nd year - (N/A) <br />
+                                3rd year - (N/A)"
+                />
+                <EduCard
+                    time="2018 - 2022"
+                    school="Fazekas Mihaly Secondary Grammar School Budapest"
+                    degree="GCSE's"
+                    comments="5 A grades in: <br />
+                     Mathematics (A - 98%), English Language (A - 93%), Computer Science (A - 96%), <br />
+                     History (A - 88%) and Hungarian Language (A - 91%)"
+                />
+                <h1 className='sectionHead certs'>
+                    Certificates
+                </h1>
+                <EduCard
+                    time="June 2023 - September 2023"
+                    school="Harvard University"
+                    degree="CS50 (online)"
+                    comments=""
+                />
+                <EduCard
+                    time="July 2022"
+                    school="Cambridge Assessment English"
+                    degree="Certificate in Advanced English"
+                    comments="Pass with an A grade and an overall score of 201. <br />
+                                CEFR level: C2"
+                />  
+            </section>
+        </a>
     )
+}
+
+function EduCard ({time, school, degree, comments}) {
+    return(
+        <section className='educard'>
+            <p className='time'>{time}</p>
+            <h2 className='school'>{school}</h2>
+            <h3 className='degree'>{degree}</h3>
+            <p className='comments' 
+                dangerouslySetInnerHTML={{ __html: comments}}></p>
+        </section>
+    )
+    
 }
 
 export default Education;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar ({showing}) {
         <nav>
             <h1 className='almostLogo'><a href=''>Konrad Vincler</a></h1>
             <ul>
-                <li><a href="#">Education</a></li>
+                <li><a href="#edu">Education</a></li>
                 <li><a href="#">Experience</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Contact</a></li>
