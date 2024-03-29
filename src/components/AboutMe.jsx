3c038c57f132f3cf4264dca87dbb7df5395c22ac
Konrad Dominik Vincler <konrad.vincler@gmail.com>
2024-03-29 00:07:20 +0000

    Implemented an About me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,10 +2,27 @@ import '../css/AboutMe.css';
 
 function AboutMe () {
     return(
-        <>
-        
-        </>
-    )
+        <section className='tile aboutme'>
+            <div className='typewriter'>
+                <h1>Hello and Welcome!</h1>
+            </div>
+            <p className='intro'>
+                My name is Konrad Dominik Vincler and currently I am 
+                a university student in my penultimate year for my Bachelors
+                degree in Computer Science. During my first year, <br /> I was able to
+                achieve a First class grade of an 89% average module grade.
+            </p>
+            <p className='intro'>
+                Apart from studying, I am striving to improve myself by gaining
+                experience through meaningful and impactful experiences.
+            </p>
+            <p className='intro'>
+                A long term aspiration of mine is being able to create web applications
+                and pieces of software that have a positive impact on millions of
+                people.
+            </p>
+        </section>
+)
 }
 
 export default AboutMe;
